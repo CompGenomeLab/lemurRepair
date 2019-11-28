29c8574afd2c517e400a646584d5eed242ed294f
Ümit Akköse <umieat@gmail.com>
2019-11-29 02:29:46 +0300

    subplots for read length distribution plot

--- oligomer_length.py
+++ oligomer_length.py
@@ -1,23 +1,64 @@
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import pandas as pd
 
 
-def plot_length(infile, outfile):
-    with open(infile) as in_handler:
+def percentage(row, reads_sum):
+    return row['count']/reads_sum*100
+
+
+def plot_length_dist(infile_human_cpd, infile_lemur_cpd, infile_human_64, infile_lemur_64, outfile):
+    fig = make_subplots(rows=2, cols=2)
+
+    with open(infile_human_cpd) as in_handler:
         df = pd.read_csv(in_handler, sep=',')
-        fig = go.Figure(
-            data=[
-                go.Bar(name='length', x=df['index'], y=df['count'], marker={'color':'#1f76b4'})
-                ],
-            layout=go.Layout(
-                title='Length distribution',
-                yaxis={'title': 'Count'},
-                xaxis={'title': 'Length'}
+        reads_sum = df['count'].sum()
+        df['read_percentage'] = df.apply(lambda row: percentage(row, reads_sum), axis=1)
+        fig.add_trace(
+            go.Bar(name='length', x=df['index'], y=df['read_percentage'], marker={'color':'#1f76b4'}),
+            row=1, col=1
             )
-        )
-        fig.write_image(outfile)
+
+    with open(infile_lemur_cpd) as in_handler:
+        df = pd.read_csv(in_handler, sep=',')
+        reads_sum = df['count'].sum()
+        df['read_percentage'] = df.apply(lambda row: percentage(row, reads_sum), axis=1)
+        fig.add_trace(
+            go.Bar(name='length', x=df['index'], y=df['read_percentage'], marker={'color':'#1f76b4'}),
+            row=1, col=2
+            )
+
+    with open(infile_human_64) as in_handler:
+        df = pd.read_csv(in_handler, sep=',')
+        reads_sum = df['count'].sum()
+        df['read_percentage'] = df.apply(lambda row: percentage(row, reads_sum), axis=1)
+        fig.add_trace(
+            go.Bar(name='length', x=df['index'], y=df['read_percentage'], marker={'color':'#1f76b4'}),
+            row=2, col=1
+            )
+
+    with open(infile_lemur_64) as in_handler:
+        df = pd.read_csv(in_handler, sep=',')
+        reads_sum = df['count'].sum()
+        df['read_percentage'] = df.apply(lambda row: percentage(row, reads_sum), axis=1)
+        fig.add_trace(
+            go.Bar(name='length', x=df['index'], y=df['read_percentage'], marker={'color':'#1f76b4'}),
+            row=2, col=2
+            )
+
+    fig.update_xaxes(title_text="Read length (nt)", row=1, col=1)
+    fig.update_xaxes(title_text="Read length (nt)", row=1, col=2)
+    fig.update_xaxes(title_text="Read length (nt)", row=2, col=1)
+    fig.update_xaxes(title_text="Read length (nt)", row=2, col=2)
+
+    fig.update_yaxes(title_text="Read count (%)", range=[0, 25], row=1, col=1)
+    fig.update_yaxes(title_text="Read count (%)", range=[0, 25], row=1, col=2)
+    fig.update_yaxes(title_text="Read count (%)", range=[0, 25], row=2, col=1)
+    fig.update_yaxes(title_text="Read count (%)", range=[0, 25], row=2, col=2)
+
+    fig.update_layout(showlegend=False, title_text="Read length distribution", height=1200, width=1600)
+    fig.write_image(outfile)
 
 
 if __name__ == "__main__":
-    plot_length("lemur_nuc_len_dist.csv", "lemur_nuc_len_dist.pdf")
-    plot_length("human_nuc_len_dist.csv", "human_nuc_len_dist.pdf")
+    plot_length_dist("human_cpd.csv", "human_64.csv", "lemur_cpd.csv", "lemur_64.csv", "fig-1.A.pdf")
